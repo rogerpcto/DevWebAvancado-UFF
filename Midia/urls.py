98ff92aedb3a53212b4a8cea86568904fb0ba260
Logan Miranda <loganmiranda@id.uff.br>
2025-01-06 00:49:48 -0300

    created urls and views

--- Midia/urls.py
+++ Midia/urls.py
@@ -5,5 +5,7 @@ app_name = "Midia"
 
 urlpatterns = [
     # we dont have views.login made yet, it's just a pattern to follow
-    path('buscar_filme', views.buscar_filme, name='buscar_filme'),
+    path("buscar_filme", views.buscar_filme, name="buscar_filme"),
+    path("buscar_serie", views.buscar_serie, name="buscar_serie"),
+    path("buscar_midia", views.buscar_midia, name="buscar_midia"),
 ]
--- Midia/views.py
+++ Midia/views.py
@@ -4,8 +4,10 @@ from django.contrib import messages
 from django.contrib.messages import constants
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
-from .models import PERFIS
+from .models import PERFIS, Midia
 import requests
+from django.http import JsonResponse
+
 
 # Create your views here.
 import os
@@ -16,13 +18,11 @@ load_dotenv()
 URL = "https://tvshow.p.rapidapi.com"
 API_HOST = "tvshow.p.rapidapi.com"
 
-HEADERS = {
-    "x-rapidapi-key": os.getenv('API_KEY'),
-    "x-rapidapi-host": API_HOST
-}
+HEADERS = {"x-rapidapi-key": os.getenv("API_KEY"), "x-rapidapi-host": API_HOST}
+
 
 def login(request):
-    
+
     if request.method == "GET":
         return render(request, "login.html")
 
@@ -30,22 +30,24 @@ def login(request):
         dados = request.POST
         email = dados.get("username")
         senha = dados.get("senha")
-        
+
         try:
             usuario = User.objects.get(email=email)
         except Exception as erro:
             usuario = None
             messages.add_message(request, constants.ERROR, "Email inválido!")
             return redirect("/")
-        
-        usuario_autenticado = auth.authenticate(request, username=usuario.username, password=senha)
-        if usuario_autenticado:  
+
+        usuario_autenticado = auth.authenticate(
+            request, username=usuario.username, password=senha
+        )
+        if usuario_autenticado:
             auth.login(request, usuario_autenticado)
             return redirect("/agenda")
         else:
             messages.add_message(request, constants.ERROR, "Senha inválida!")
             return redirect("/")
-        
+
 
 def criar_conta(request):
     if request.method == "GET":
@@ -58,43 +60,116 @@ def criar_conta(request):
         email = dados.get("email").strip()
         senha = dados.get("senha")
 
-        if User.objects.filter(email=email).exists() or User.objects.filter(username=email).exists():
-            messages.add_message(request, constants.WARNING, "Já existe um usuário com esse email")
+        if (
+            User.objects.filter(email=email).exists()
+            or User.objects.filter(username=email).exists()
+        ):
+            messages.add_message(
+                request, constants.WARNING, "Já existe um usuário com esse email"
+            )
 
         else:
             try:
-                User.objects.create_user(username=email, first_name=nome, perfil= PERFIS.USUARIO, email=email, password=senha)
-                messages.add_message(request, constants.SUCCESS, "Conta criada com sucesso!")
+                User.objects.create_user(
+                    username=email,
+                    first_name=nome,
+                    perfil=PERFIS.USUARIO,
+                    email=email,
+                    password=senha,
+                )
+                messages.add_message(
+                    request, constants.SUCCESS, "Conta criada com sucesso!"
+                )
 
             except Exception as erro:
                 print(erro)
-                messages.add_message(request, constants.ERROR, "Não foi possível criar conta")
-                # return redirect("/")  
-    
+                messages.add_message(
+                    request, constants.ERROR, "Não foi possível criar conta"
+                )
+                # return redirect("/")
+
         return redirect("/")
-    
+
+
 def buscar_filme(request):
 
     if request.method == "GET":
         return render(request, "buscar_filme.html")
 
-
     if request.method == "POST":
         url = f"{URL}/Movie/Search"
         params_to_search = ["Content"]
-        query_string = {"Page":"1","Language":"en-US","Adult":"true"}
+        # TO DO: if the media is not in the first page, then we can add a feature to load more medias
+        query_string = {"Page": "1", "Language": "pt-BR", "Adult": "true"}
         dados = request.POST
         for param in params_to_search:
             if dados.get(param):
                 query_string[param] = dados.get(param)
-                
-        response = requests.get(url, headers= HEADERS, params=query_string)
+
+        response = requests.get(url, headers=HEADERS, params=query_string)
         json_data = response.json()
         medias = []
-        properties_to_filter = ["title", "releaseDate", "voteAverage", "genres"]
+        properties_to_filter = ["title", "releaseDate", "voteAverage", "genres", "id"]
         for media in json_data:
             if media["originalLanguage"] == "en":
-                filtered_media = {key: media[key] for key in properties_to_filter if key in media}
+                filtered_media = {
+                    key: media[key] for key in properties_to_filter if key in media
+                }
                 medias.append(filtered_media)
+        print(medias)
 
         return render(request, "buscar_filme.html", {"medias": medias})
+
+
+def buscar_serie(request):
+    if request.method == "GET":
+        return render(request, "buscar_filme.html")
+
+    if request.method == "POST":
+        dados = request.POST
+        url = f"{URL}/Serie/Search"
+        params_to_search = ["Content"]
+        # TO DO: if the media is not in the first page, then we can add a feature to load more medias
+        if dados.get("api_externa"):
+            query_string = {"Page": "1", "Language": "pt-BR", "Adult": "true"}
+            for param in params_to_search:
+                if dados.get(param):
+                    query_string[param] = dados.get(param)
+
+            response = requests.get(url, headers=HEADERS, params=query_string)
+            json_data = response.json()
+            medias = []
+            properties_to_filter = [
+                "name",
+                "releaseDate",
+                "voteAverage",
+                "genres",
+                "id",
+            ]
+            for media in json_data:
+                if media["originalLanguage"] == "en":
+                    filtered_media = {
+                        key: media[key] for key in properties_to_filter if key in media
+                    }
+                    medias.append(filtered_media)
+
+        else:
+            url = "http://localhost:8000/buscar_midia"  # Ajuste o domínio conforme necessário
+            params = {"nome": dados.get("Content")}  # Parâmetro de consulta
+            medias = requests.get(url, params=params).json()
+        return render(request, "buscar_filme.html", {"medias": medias})
+
+
+def buscar_midia(request):
+    if request.method == "GET":
+        nome = request.GET.get("nome", "")
+        midias = Midia.objects.filter(titulo=nome)
+        if not nome:
+            midias = Midia.objects.all()
+        midias = list(midias.values())
+        if not midias:
+            return JsonResponse(
+                {"message": "Mídias não encontradas"},
+                status=404,  # Código HTTP para "Não Encontrado"
+            )
+        return JsonResponse(midias, safe=False)
