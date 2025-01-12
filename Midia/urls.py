2b792e8bddeca6e88b5e57aea60e9d13b404e43a
Logan Miranda <loganmiranda@id.uff.br>
2025-01-12 02:38:15 -0300

    created endpoints related to review

--- Midia/urls.py
+++ Midia/urls.py
@@ -11,6 +11,13 @@ urlpatterns = [
     path("buscar_midia", views.buscar_midia, name="buscar_midia"),
     path("salvar_midia", views.salvar_midia, name="salvar_midia"),
     path("criar_review", views.criar_review, name="criar_review"),
+    path("review", views.review, name="review"),
+    path("get_review", views.get_review, name="get_review"),
+    path(
+        "get_details_review/<int:id_review>/",
+        views.get_details_review,
+        name="details_review",
+    ),
     path("login", views.login, name="login"),
     path("logout", views.logout, name="logout"),
     path("criar_conta", views.criar_conta, name="criar_conta"),
